ad4a3e9e2126de433d663df4138beb4702e2c65e
Alexandr <alexandrzhila@mail.ru>
2023-03-21 22:31:56 +0300

    Updating Collection.java

--- src/main/java/org/example/Collection.java
+++ src/main/java/org/example/Collection.java
@@ -1,5 +1,10 @@
 package org.example;
+import java.util.LinkedHashSet;
 
 public class Collection {
+    private LinkedHashSet<Flat> collection;
 
+    public Collection(LinkedHashSet<Flat> collection) {
+        this.collection = collection;
+    }
 }
