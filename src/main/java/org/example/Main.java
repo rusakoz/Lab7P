46af786476afb38b5c7f8acd62c0dda50dfd7836
Руслан <julyrak@gmail.com>
2023-03-23 20:06:27 +0300

    add dotenv

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,11 @@
 package org.example;
 
+import io.github.cdimascio.dotenv.Dotenv;
 public class Main {
     public static void main(String[] args) {
         InputOutput Output = new InputOutput();
         Output.Output("Ку-ку ептаа");
+        Dotenv dotenv = Dotenv.load();
+        System.out.println(dotenv.get("HELLO"));
     }
 }
